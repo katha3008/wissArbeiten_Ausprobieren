61e34323547e6ed794854f41c765daf0016d3b83
katha3008 <katha.hautzinger@gmx.de>
2025-12-28 20:53:25 +0100

    Kontrolle der Argumente fuer erste Funktion hinzugefuegt

--- diskrete Gleichverteilung.R
+++ diskrete Gleichverteilung.R
@@ -1,7 +1,11 @@
 ## meine Funktionen fuer die diskrete Gleichverteilung
 
 my_dunifdis <- function(x, a, b) {
-  d <- rep(1/(b-a+1), length(x))
+  if(!is.numeric(a) | !is.numeric(b) | !is.numeric(x)) {stop("x, a und b muessen numerische Argumente sein")}
+  if(length(a) != 1 | length(b) != 1) {stop("a und b sollen keine Vektoren sein")}
+  if(round(a) != a | round(b) != b) {stop("b und a muessen ganze Zahlen sein")}
+  if(a >= b) {stop("b muss echt groesser a sein.")}
+  d <- ifelse(x %in% a:b, 1/(b-a+1), 0)
   return(d)
 }
 
@@ -21,11 +25,11 @@ my_runifdis <- function(n, a, b) {
   return(r)
 }
 
-b <- 12
-a <- 3
+#b <- 12
+#a <- 3
 
-my_dunifdis(3:12, a, b)
-my_punifdis(3:12, a, b)
-my_qunifdis((1:20)/20, a, b)
-y <- my_runifdis(100000, a, b)
-table(y)
+# my_dunifdis(3:12, a, b)
+# my_punifdis(3:12, a, b)
+# my_qunifdis((1:20)/20, a, b)
+# y <- my_runifdis(100000, a, b)
+# table(y)
